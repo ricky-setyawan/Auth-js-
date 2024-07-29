b45a250cf2b6a96ca5c9b183baa52a92201dc96c
uwewerner244 <uwewerner244@gmail.com>
2024-07-29 11:09:15 -0700

    update React-auth

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,43 @@
+import React, { useContext, useState, useEffect } from 'react' 
+// useEffect
+import { auth } from "../firebase"
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
+
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setLoading(false)
+            setCurrentUser(user)
+        })
+
+        return unsubscribe
+    }, [])
+
+    auth.onAuthStateChanged(user => {
+        setCurrentUser(user)
+    })
+
+    const value = {
+        currentUser,
+        signup
+    }
+
+  return (
+    <AuthContext.Provider value={value}>
+        {!loading && children }
+    </AuthContext.Provider>
+  )
+}
+
